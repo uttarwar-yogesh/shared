b301af248d9704fae2ba263493713035370c1ed5
uttarwar-yogesh <34362984+uttarwar-yogesh@users.noreply.github.com>
2021-04-07 15:07:42 +0530

    Update repoCloning.groovy

--- vars/repoCloning.groovy
+++ vars/repoCloning.groovy
@@ -1,6 +1,7 @@
 def call(String repoUrl, String BRANCH) { 
 pipeline {
     agent any 
+	stages{
         stage ('Code Checkout') {
         steps{ 
 	     git branch: "${BRANCH}",
@@ -8,4 +9,5 @@ pipeline {
             }            
         }
     }
+  }
 }
