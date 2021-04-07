565a0eabaf5852a539e576fe431956bce3a0b096
uttarwar-yogesh <34362984+uttarwar-yogesh@users.noreply.github.com>
2021-04-07 15:05:31 +0530

    Create repoCloning.groovy

---
+++ vars/repoCloning.groovy
@@ -0,0 +1,11 @@
+def call(String repoUrl, String BRANCH) { 
+pipeline {
+    agent any 
+        stage ('Code Checkout') {
+        steps{ 
+	     git branch: "${BRANCH}",
+             url: "${repoUrl}"
+            }            
+        }
+    }
+}
