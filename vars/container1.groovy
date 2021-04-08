ec8068542750d0a1cba3626af087c676a7cd9b07
uttarwar-yogesh <34362984+uttarwar-yogesh@users.noreply.github.com>
2021-04-08 15:56:47 +0530

    Update container1.groovy

--- vars/container1.groovy
+++ vars/container1.groovy
@@ -50,9 +50,9 @@ pipeline {
 		        // sh scriptFileContent
 			// './build-base-image.sh' "${JDK_VERSION}" "${MVN_VERSION}" "${WL_VERSION}" "${PATCH_VERSION}" "${PATCH_PATH}"
 			
-			// def cmd = [ "/bin/bash", "-c", "build-base-image.sh", "${JDK_VERSION}", "${MVN_VERSION}", "${WL_VERSION}", "${PATCH_VERSION}", "${PATCH_PATH}" ]
+			def cmd = [ "/bin/bash", "-c", "build-base-image.sh", '''"${JDK_VERSION}", "${MVN_VERSION}", "${WL_VERSION}", "${PATCH_VERSION}", "${PATCH_PATH}"''' ]
 				
-			def cmd = [ "/bin/bash", "-c", "build-base-image.sh", '+JDK_VERSION+' '+MVN_VERSION+' '+WL_VERSION+' '+PATCH_VERSION+' '+PATCH_PATH+' ]
+			// def cmd = [ "/bin/bash", "-c", "build-base-image.sh", '+JDK_VERSION+' '+MVN_VERSION+' '+WL_VERSION+' '+PATCH_VERSION+' '+PATCH_PATH+' ]
 			def sout = new StringBuffer()
 			def serr = new StringBuffer()	
 			
