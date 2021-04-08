4a192b36b40aad5e82253c213e81e851a5c9f382
uttarwar-yogesh <34362984+uttarwar-yogesh@users.noreply.github.com>
2021-04-08 15:21:48 +0530

    Update container1.groovy

--- vars/container1.groovy
+++ vars/container1.groovy
@@ -44,12 +44,16 @@ pipeline {
 	   		 '''
 			// File script = new File('./var/lib/jenkins/workspace/New-container/build-base-image.sh "${JDK_VERSION}" "${MVN_VERSION}" "${WL_VERSION}" ')
 			// script.getText().execute()
-			 "sh ./build-base-image.sh".execute()
-			echo "executed"
+			// "sh ./build-base-image.sh".execute()
 			// def scriptFileContent = libraryResource( '/var/lib/jenkins/workspace/New-containerbuild-base-image.sh' )
 		        // sh scriptFileContent
 			// './build-base-image.sh' "${JDK_VERSION}" "${MVN_VERSION}" "${WL_VERSION}" "${PATCH_VERSION}" "${PATCH_PATH}"
 			
+			def cmd = [ "build-base-image.sh", "${JDK_VERSION}" , "${MVN_VERSION}", "${WL_VERSION}", "${PATCH_VERSION}", "${PATCH_PATH}" ]
+				
+			cmd.execute()
+			echo "Executed"
+				
 			}
 			else
 			{
