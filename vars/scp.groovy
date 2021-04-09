0ea01c57f854e5e3bfd42f74e35aba844e0a3f52
uttarwar-yogesh <34362984+uttarwar-yogesh@users.noreply.github.com>
2021-04-09 15:46:00 +0530

    Update scp.groovy

--- vars/scp.groovy
+++ vars/scp.groovy
@@ -1,11 +1,10 @@
 def call(String hostname, String targethostname, String username, String password, String src, String dest ) { 
 node {
         stage ('Coping files') {
-        steps{
+
 	    sh '''
 	    rsync --rsh="sshpass -p  password ssh -o StrictHostKeyChecking=no -l username" hostname:src targethostname:dest
-		'''
-       }
+            '''
      }
    }
  }
