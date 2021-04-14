c76270f0373820f52ff968b9491fb05145f0c758
uttarwar-yogesh <34362984+uttarwar-yogesh@users.noreply.github.com>
2021-04-14 14:43:53 +0530

    Update e_mail.groovy

--- vars/e_mail.groovy
+++ vars/e_mail.groovy
@@ -5,6 +5,8 @@ def notify(status) {
        body: """
        Check console output at <a href="${env.BUILD_URL}">${env.JOB_NAME} (${env.BUILD_NUMBER})</a>""",
        to: "${BUILD_USER_EMAIL}",
+        
        from: 'jenkins@company.com')
    }
+   println ${BUILD_USER_EMAIL}
 }
