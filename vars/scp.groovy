54858b97f85d746adefea4fe33ba8fdb9c33fcb6
uttarwar-yogesh <34362984+uttarwar-yogesh@users.noreply.github.com>
2021-04-16 12:07:10 +0530

    Update scp.groovy

--- vars/scp.groovy
+++ vars/scp.groovy
@@ -1,8 +1,28 @@
-def call(String hostname, String targethostname, String username, String password, String src, String dest ) { 
-node {
-	echo env.hostname
-	echo evn.targethostname
+
+def call(String hostname, String targethostname, String username, String password, String src, String dest ) {
 	
+properties([
+     
+        parameters ([
+          
+            string(name: 'hostname', defaultValue: '', description: 'Provide source hostname'),
+            string(name: 'targethostname', defaultValue: '', description: 'Provide target hostname'),
+            string(name: 'username', defaultValue: '', description: 'Provide username'),
+            string(name: 'password', defaultValue: '', description: 'Provide password'),
+            string(name: 'src', defaultValue: '', description: 'provide src path'),
+            string(name: 'dest', defaultValue: '', description: 'provide dest path'),
+        ])
+    ])
+	
+node {
+	{
+	hostname = "${params.hostname}"
+	targethostname = "${params.targethostname}"
+	username = "${params.username}"
+	password = "${params.password}"
+	src = "${params.src}"
+	dest = "${params.dest}"
+	}
         stage ('Coping files') {
 
 		sh 'rsync --rsh="sshpass -p  ${password} ssh -o StrictHostKeyChecking=no -l ${username}" ${hostname}:${src} ${targethostname}:${dest}'
