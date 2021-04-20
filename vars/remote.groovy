4d58f14d3ec96ee0a696205c6d4b7e29e7de3e69
uttarwar-yogesh <34362984+uttarwar-yogesh@users.noreply.github.com>
2021-04-20 14:58:27 +0530

    Update remote.groovy

--- vars/remote.groovy
+++ vars/remote.groovy
@@ -1,2 +1,2 @@
-def remote = [name: 'test', host: 'localhost', user: 'yuttarwar', password: "bumpyShow72@", allowAnyHosts: true]
-                     sshCommand remote: remote, command: "for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done"
+def call() { [name: 'test', host: 'localhost', user: 'yuttarwar', password: "bumpyShow72@", allowAnyHosts: true]
+                     sshCommand remote: remote, command: "for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done" } 
