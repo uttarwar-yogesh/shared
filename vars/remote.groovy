4f5e5f3ae0df2d15b8dff722d16ae826cf98eb73
uttarwar-yogesh <34362984+uttarwar-yogesh@users.noreply.github.com>
2021-04-20 15:31:05 +0530

    Update remote.groovy

--- vars/remote.groovy
+++ vars/remote.groovy
@@ -1,6 +1,14 @@
-def call() { 
-  script{
-      remote1 = [name: 'test', host: 'localhost', user: 'yuttarwar', password: "@", allowAnyHosts: true]
-              sshCommand remote: remote, command: "for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done" 
+def remote = [:]
+remote.name = "hostname"
+remote.host = "localhost"
+remote.allowAnyHosts = true
+node {
+  withCredentials([sshUserPrivateKey(credentialsId: 'eedb5f06-cb33-4352-a9ea-c28b8a4f35a7', keyFileVariable: 'identity', passphraseVariable: 'passphrase', usernameVariable: 'userName')]) {
+  remote.user = userName
+    remote.identityFile = identity
+  remote.passphrase = passphrase
+  stage("SSH Steps Rocks!") {
+    writeFile file: 'abc.sh', text: 'ls'
+    sshPut remote: remote, from: 'abc.sh', into: '.'
   }
-} 
+}
